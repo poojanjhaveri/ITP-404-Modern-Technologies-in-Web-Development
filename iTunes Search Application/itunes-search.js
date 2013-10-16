a6360b16328104a7c8ac8780f7788fa5544f69aa
Poojan Jhaveri <poojanjhaveri@hotmail.com>
2013-10-15 17:34:08 -0700

    Itunes-Search-Application Complete
    Implemented a custom handle-bar register helper. That did the trick.
    
    Other possible option - parse the jsonp and use the objectname as a
    class and change the visibility there

--- iTunes Search Application/itunes-search.js
+++ iTunes Search Application/itunes-search.js
@@ -1,14 +1,33 @@
 // JavaScript Document
 
+var templateString = $('#itunes-search-page-template').html();
+var templateFunction = Handlebars.compile(templateString);
+
+
+Handlebars.registerHelper('if_custom', function(a, b, opts) {
+	
+    if(a.trackExplicitness == "notExplicit")
+	{
+        return opts.fn(this);  // Not Explicit
+	}
+    else
+	{
+        return opts.inverse(this); // Explicit
+	}
+});
+
+
 $( "#itunes-search" ).submit(function( event ) {
   event.preventDefault();
   
-   var searchtext=$('#search-text').val();            // Get Search text
+   var searchtext=$('#search-text').val();           // Get Search text
    searchtext=searchtext.replace(" ","+");			// Replace " " with + for the API. Eg jack+johnson
-   console.log(searchtext);	
    jsonRequest("https://itunes.apple.com/search?term="+searchtext+"&entity=song&limit=30&callback=jsonHandler"); // Request
 });
 
+/*
+Function for JSON Request
+*/
 function jsonRequest(url)
 {
      var script=document.createElement('script');
@@ -16,13 +35,26 @@ function jsonRequest(url)
      document.getElementsByTagName('head')[0].appendChild(script);
 }
 
+/*
+Callback function after the request
+*/
 function jsonHandler(data)
 {
-	console.log(data);
-    if (data) {
-         var htmlWithData = templateFunction(data);
-    
-    $('#content').append(htmlWithData);
-    }
+		var $content = $('#content');			// selector caching
+		$content.html("");						// Erase the content at beginning of every json Request
+		console.log(data);
+		if(data.results.length==0)				// Results count is zero. So no data obtained
+		{
+			$content.html("No artist found");		// No vaid artist found
+			}
+		else
+		{
+		  for(var i=0;i<data.results.length;i++)
+		  {
+			 var htmlWithData = templateFunction(data.results[i]);	 // pass the data to the function so that it compiles
+			 $content.append(htmlWithData);					// Append the data to existing div
+		  }
+		}
+
    
 }
